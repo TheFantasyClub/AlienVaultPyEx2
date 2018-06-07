fd9a625d32e04d2364459f11faf1e61af1c736bb
TheFantasyClub <106141+TheFantasyClub@users.noreply.github.com>
2018-06-08 00:20:34 +0200

    [MOD] Issue with python 2.7 and 3.3

--- tests/test_AlienVaultPyEx2.py
+++ tests/test_AlienVaultPyEx2.py
@@ -29,9 +29,9 @@ class TestDictIssuesComposer(unittest.TestCase):
         a = ListComposer(self.id, self.state, self.title,
                          self.repository, self.created_at)
         out_a = json.dumps(a.GetListComposer())
-        assert(out_a == '{"id": 38, "state": "open", "title": "Found a bug", '
-                        + '"repository": "own1/repo1", "created_at":'
-                        + ' "2011-04-22T13:33:48Z"}')
+        assert(out_a == ('{"id": 38, "state": "open", "title": "Found a bug", '
+                         + '"repository": "own1/repo1", "created_at":'
+                         + ' "2011-04-22T13:33:48Z"}'))
 
     def tearDown(self):
         pass
