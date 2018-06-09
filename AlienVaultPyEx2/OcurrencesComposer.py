07959ce9091db6d6737f21ae750762fc3b79c7b5
TheFantasyClub <106141+TheFantasyClub@users.noreply.github.com>
2018-06-09 19:46:18 +0200

    [MOD] Reordering OcurrencesComposer.py to use into the OcurrencesComposer class

--- AlienVaultPyEx2/OcurrencesComposer.py
+++ AlienVaultPyEx2/OcurrencesComposer.py
@@ -1,21 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-class OcurrencesComposer(object):
-    """
-    Creates a dictionary with only one element:
-    the last and most issues ocurrence counter
-    """
-
-    def __init__(self, issue_list=None):
-        self.dictocurrences = dict()
-        if issue_list is not None:
-            self.dictocurrences = {issue_list}
-
-    def OutputData(self):
-        return self.dictocurrences
-
-
 def gen_dict_extract(key, var):
     """ https://stackoverflow.com/questions/9807634/find-all-occurrences-of-a-key-in-nested-python-dictionaries-and-lists """
     if hasattr(var, 'items'):
@@ -55,3 +40,18 @@ def SplitAListGen(l, n):
     """https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks"""
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+
+class OcurrencesComposer(object):
+    """
+    Creates a dictionary with only one element:
+    the last and most issues ocurrence counter
+    """
+
+    def __init__(self, issue_list=None):
+        self.dictocurrences = dict()
+        if issue_list is not None:
+            self.dictocurrences = {issue_list}
+
+    def OutputData(self):
+        return self.dictocurrences
